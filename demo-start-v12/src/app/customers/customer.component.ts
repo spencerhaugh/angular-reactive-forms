a0e0d960920661816b1acd644b26e4401b832740
Spencer Haugh <spencer.haugh@gmail.com>
2022-01-03 14:23:30 -0500

    implement addAddress method and refactor HTML with FormArray to add multiple addresses as needed

--- demo-start-v12/src/app/customers/customer.component.ts
+++ demo-start-v12/src/app/customers/customer.component.ts
@@ -70,6 +70,10 @@ export class CustomerComponent implements OnInit {
     );
   }
 
+  addAddress(): void {
+    this.addresses.push(this.buildAddress());
+  }
+
   buildAddress(): FormGroup {
     return this.fb.group({
       addressType: 'home',
